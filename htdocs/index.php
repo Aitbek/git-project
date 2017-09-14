d04dbc4fde3215ce2edc513146d0f3d62c24fbfb
a_anarkulov <a.anarkuloff@gamil.com>
2017-09-14 15:21:52 +0600

    commit in to new branch

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,10 +1,5 @@
-<?php 
-## Перебор списка
-$dossier = [
-	["name" => "Thomas Anderson", "born" => "1962-03-11"],
-	["name"=> "Keanu Reeves", "born"=> "1962-09-02"],
-];
-for($i = 0; $i < count($dossier); $i++)
-	echo "{$dossier[$i]['name']} was born {$dossier [$i]['born']}<br />"; 
+<?php
 
-echo "Hello git!";
+    echo "Hello Git!";
+    echo "Hello Git2!";
+    echo "Hello new branch!";
